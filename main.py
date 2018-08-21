ed8ea4a42934fa4cc97ff3ed8a5ae9480d2185fd
Dale Erikson <dje23@pitt.edu>
2018-08-21 14:30:22 -0400

    Added some reactions, Commented ideas into main.py

--- main.py
+++ main.py
@@ -4,21 +4,44 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit.Chem import Draw
 
+
+def isStartingMaterial(molecule):
+    # import list of acceptable starting materials
+    # iterate through to confirm mol (e.g. Benzene)
+    # OR traverse mol graph to confirm all hydrocarbons
+    return True
+    # or False later.....
+
+def molBreakdown(mainMol, reactLib): # Forseeing issue of competitive rxns; make priority queue of best reactions?
+    # Recursively breakdown molecule via a BST until all leaf nodes
+    # are consistent with starting materials
+
+
+    # is mainMol starting?
+        # no? find applicable reaction
+            # branch left and right
+            # if no reaction applicable to
+
+    # Add debug tool to print depth of tree, make flag 
+
+
+
 if(len(arg) != 2):
-    print('Requires argument: .smi file')
+    print('Requires argument: .smi file of select molecule')
 else:
     if (arg[1].endswith('.smi')) == False:
         print('Requires .smi file to read..')
     else:
         with open(arg[1],'r') as molFile:
-            orgMol = molFile.read()
-            mainMol = Chem.MolFromSmiles(orgMol)
-            #rxn = AllChem.ReactionFromSmarts()
-            #reactant = []
-            #reactant.append(mainMol)
-            #product = rxn.RunReactants(reactant)
-            #for prod in product:
-                #for pair in prod:
-                    #print(AllChem.MolToSmiles(pair))
-            
-            Draw.MolToFile(mainMol,'./' + arg[1].strip('.smi') + '.png')
+            orgMol = molFile.read()           # FUTURE: Allow user to draw molecule to deconstruct using other lib
+            mainMol = Chem.MolFromSmiles(orgMol) # !!!! Make smi mol supplier object
+            reactLib = [] # read reaction file library and store as dictionary
+                                # key: str of reaction name
+                                # value: reaction object
+
+            try:
+                molBreakdown(mainMol, reactLib)
+            except Exception as e:
+                print("Error: " + e)
+        print("Done!")
+        # Maybe add feature to print image depicting deconstruction of molecule
